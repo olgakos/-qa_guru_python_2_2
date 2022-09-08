4192868ed7710b78ad97baa1579419d04b603726
Olga <ok2304@yandex.ru>
2022-09-08 13:23:18 +0300

    Два теста на поиск в Google

---
+++ google_simple_tests_selene.py
@@ -0,0 +1,36 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def browser_open_baseURL():
+    browser.open('https://google.com')
+    yield
+
+
+@pytest.fixture(scope="session")
+def browser_open_setting():
+    browser.config.window_width = 800
+    browser.config.window_height = 600
+    yield
+
+
+@pytest.fixture(scope="session")
+def browser_open_setting2():
+    browser.config.window_width = 1024
+    browser.config.window_height = 768
+    yield
+
+
+def test_google_positive(browser_open_setting):
+    # browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_google_negative(browser_open_setting2):
+    # browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    #browser.element('[id="search"]').should(have.text('To be, or not to be'))
+    browser.element('[id="search"]').should(have.no.text('To be, or not to be'))
